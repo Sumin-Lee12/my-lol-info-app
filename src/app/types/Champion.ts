2cc9adb618f6772630655e6a42c57361e177359d
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-12 03:21:59 +0900

    feat: 각 페이지를 위한 타입 선언

---
+++ src/app/types/Champion.ts
@@ -0,0 +1,32 @@
+export type ChampionImage = { // 이것도 export가 되야하나?
+  full: string;
+  sprite: string;
+  group: string;
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+}
+
+export type Champion = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: ChampionImage;
+}
+
+export type ChampionDetail = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  lore: string;
+  info: {
+    attack: number,
+    defense: number,
+    magic: number,
+    difficulty: 4
+    },
+  image: ChampionImage;
+}
---
+++ src/app/types/ChampionRotation.ts
@@ -0,0 +1,9 @@
+import { ChampionImage } from "./Champion";
+
+export type Item = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: ChampionImage;
+}
