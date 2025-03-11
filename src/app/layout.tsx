7892c9968b0f9e5855eef332e5a1ac3fb1cf4172
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-11 19:46:37 +0900

    프로젝트 생성 및 셋업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <ul>
+            <li><Link href={"/"}>Home</Link></li>
+            <li><Link href={"/champions"}>Champions</Link></li>
+            <li><Link href={"/items"}>Items</Link></li>
+            <li><Link href={"/rotation"}>Rotation</Link></li>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
