7e63342765b89ef2c750eddc2d4ef24018894ed0
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-12 05:45:48 +0900

    feat: 아이템 페이지 SSG방식 fetch 구현

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,5 +1,32 @@
-export default function page () {
+// import { Card } from "../components/card";
+// import type {CardProps} from "../components/card";
+import type { Champion } from "../types/Champion";
+import Image from "next/image";
+
+export default async function itemPage () {
+  const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json", {
+    cache: "force-cache"
+  });
+  const json = await res.json();
+  const data: Champion[] = Object.values(json.data);
+  console.log(data);
+
   return (
-    <div>items page</div>
+    <div className="min-h-screen w-full grid grid-cols-4 justify-items-center gap-5">
+      {data.map((champion) => (
+          <div className="flex flex-col justify-center items-center w-full h-[300px] border">
+            {/* <Image
+              src={champion.image.sprite}
+              alt={champion.image.full}
+              width={champion.image.w}
+              height={champion.image.h}
+            /> */}
+            <h3>{champion.name}</h3>
+            <p>{champion.title}</p>
+          </div>
+        )
+      )}
+    </div>
   )
 }
+
