eb8d890974c71cc7082ab094e22fc5e5ee3b3640
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-12 17:55:29 +0900

    feat: 아이템 & 챔피언 페이지 데이터 fetch 완료 (+ 기본 레이아웃)

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,45 @@
-export default function page () {
+"use client";
+
+import React, { useState, useEffect } from "react";
+import type { Champion } from "../types/Champion";
+import Image from "next/image";
+import { RiotChampionImageLink } from "../components/imageLink";
+
+export default function ItemPage() {
+  const [data, setData] = useState<Champion[]>([]);
+
+  useEffect(() => {
+    const fetchChampions = async () => {
+      const res = await fetch(
+        "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json"
+      );
+      const json = await res.json();
+      const data: Champion[] = Object.values(json.data);
+      setData(data);
+    };
+    fetchChampions();
+  }, []);
+
   return (
-    <div>champions page</div>
-  )
+    <div className="min-h-screen w-full justify-items-center mx-5">
+      <h1 className="font-bold text-[32px] my-20">챔피언 목록</h1>
+      <div className="min-h-screen w-full grid grid-cols-4 justify-items-center gap-5">
+        {data.map((champion) => (
+          <div
+            key={champion.id}
+            className="flex flex-col justify-center items-center w-full h-[300px] border-gray-600 border rounded-lg"
+          >
+            <Image
+              src={`${RiotChampionImageLink}${champion.image.full}`}
+              alt={champion?.image?.full}
+              width={champion?.image?.w}
+              height={champion?.image?.h}
+            />
+            <h3 className="font-bold text-[32px]">{champion.name}</h3>
+            <p>{champion.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,32 +1,35 @@
 // import { Card } from "../components/card";
 // import type {CardProps} from "../components/card";
-import type { Champion } from "../types/Champion";
+import type { Item } from "../types/Items";
 import Image from "next/image";
-import { RiotImageLink } from "../components/imageLink";
+import { RiotItemImageLink } from "../components/imageLink";
 
 export default async function itemPage () {
-  const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json", {
+  const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json", {
     cache: "force-cache"
   });
   const json = await res.json();
-  const data: Champion[] = Object.values(json.data);
+  const data: Item[] = Object.values(json.data);
   console.log(data);
 
   return (
-    <div className="min-h-screen w-full grid grid-cols-4 justify-items-center gap-5">
-      {data.map((champion) => (
-          <div className="flex flex-col justify-center items-center w-full h-[300px] border">
-            <Image
-              src={`${RiotImageLink}${champion.image}`}
-              alt={champion?.image?.full}
-              width={champion?.image?.w}
-              height={champion?.image?.h}
-            />
-            <h3>{champion.name}</h3>
-            <p>{champion.title}</p>
-          </div>
-        )
-      )}
+    <div className="min-h-screen w-full justify-items-center mx-5">
+      <h1 className="font-bold text-[32px] my-20">아이템 목록</h1>
+      <div className="min-h-screen w-full grid grid-cols-4 justify-items-center gap-5">
+        {data.map((Item) => (
+            <div key={Item.id} className="flex flex-col justify-center items-center w-full h-[300px] border-gray-600 border rounded-lg">
+              <Image
+                src={`${RiotItemImageLink}${Item.image.full}`}
+                alt={Item?.image?.full}
+                width={Item?.image?.w}
+                height={Item?.image?.h}
+              />
+              <h3 className="font-bold text-[32px]">{Item.name}</h3>
+              <p>{Item.title}</p>
+            </div>
+          )
+        )}
+      </div>
     </div>
   )
 }
