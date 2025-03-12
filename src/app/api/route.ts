be3e41fc07bb3aa9a8757ac1ed4b8b7726845274
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-12 16:55:12 +0900

    Rename: route.js를 .ts로 파일명 수정

---
+++ src/app/api/route.ts
@@ -0,0 +1,17 @@
+export type headers = {
+  "Content-Type": string;
+  "API_KEY": string;
+}
+
+export async function GET () {
+  const res = await fetch ("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json", {
+    headers:  { //???????여기 headers 타입을 넣는 건가? 왜 빨간 오류가 안 사라지지?
+      "Content-Type": "application/json",
+      'API-KEY': process.env.RIOT_API_KEY,
+    },
+  });
+  const data = await res.json();
+
+  return Response.json({data});
+}
+
