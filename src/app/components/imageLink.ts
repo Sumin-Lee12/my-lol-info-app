1f7504196f8a58c8038c715b89622eab6511b62a
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-12 17:57:30 +0900

    fix: 이미지 fetch가 안되던 이슈 해결

--- src/app/components/imageLink.ts
+++ src/app/components/imageLink.ts
@@ -1,1 +1,5 @@
-export const RiotImageLink = "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/"
+export const RiotItemImageLink =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/";
+
+export const RiotChampionImageLink =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/";
