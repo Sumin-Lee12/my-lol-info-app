193d952d3ee29a68b1173e5387434264b9788311
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-17 02:06:11 +0900

    design: 홈 화면 UI 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,16 +29,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-full min-h-screen grid grid-rows-[1, 1fr]`}
       >
-        <nav className="bg-gray-700 text-white mb-5">
+        <nav className="bg-black border-b border-white text-white">
           <ul className="flex flex-row justify-around items-center my-5">
-            <li><Link href={"/"}>Home</Link></li>
-            <li><Link href={"/champions"}>Champions</Link></li>
-            <li><Link href={"/items"}>Items</Link></li>
-            <li><Link href={"/rotation"}>Rotation</Link></li>
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/champions"}>Champions</Link>
+            </li>
+            <li>
+              <Link href={"/items"}>Items</Link>
+            </li>
+            <li>
+              <Link href={"/rotation"}>Rotation</Link>
+            </li>
           </ul>
         </nav>
         <div className="min-h-screen w-full flex justify-center items-center">
-        {children}
+          {children}
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,24 @@ import Link from "next/link";
 import Image from "next/image";
 import characterImage from "./public/lolCharacter.jpg";
 import characterRotationImage from "./public/lolChampion.jpg";
-import itemImage from "./public/lolItems.webp"
+import itemImage from "./public/lolItems.webp";
+import homepageImage from "./public/lol-hompg-img.jpg";
 
 export default function page() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="mb-20">Home Page</h1>
-      <div>
+    <div className="flex flex-col justify-center items-center bg-black">
+      <div className="relative w-screen h-screen">
+        <Image
+          src={homepageImage}
+          alt="LoL Home Image"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-300 to-gray-950 mix-blend-multiply opacity-80"></div>
+        <h1 className="absolute inset-0 flex justify-center items-center text-white text-5xl font-bold z-10">
+          League of Legends
+        </h1>
+      </div>
+      <div className="text-white">
         <Link href={"/champions"}>
           <div className="justify-items-center mb-60">
             <Image
@@ -19,7 +30,7 @@ export default function page() {
               className="mb-5"
             />
             <p>챔피언 목록보기</p>
-          </div>        
+          </div>
         </Link>
         <Link href={"/items"}>
           <div className="justify-items-center mb-60">
@@ -31,7 +42,7 @@ export default function page() {
               className="mb-5"
             />
             <p>아이템 목록보기</p>
-          </div>        
+          </div>
         </Link>
         <Link href={"/rotation"}>
           <div className="justify-items-center mb-60">
@@ -43,9 +54,9 @@ export default function page() {
               className="mb-5"
             />
             <p>이번 주 무료 챔피언 목록보기</p>
-          </div>        
+          </div>
         </Link>
       </div>
     </div>
-  )
+  );
 }
