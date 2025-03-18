b95782e13d96cf38463c1036114bdb3f5a7d473d
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-18 14:33:23 +0900

    test: route.ts 주석 처리

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,31 @@
-export type headers = {
-  "Content-Type": string;
-  "API_KEY": string;
-}
+// export type headers = {
+//   "Content-Type": string;
+//   API_KEY: string;
+// };
 
-export async function GET () {
-  const res = await fetch ("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json", {
-    headers:  { //???????여기 headers 타입을 넣는 건가? 왜 빨간 오류가 안 사라지지?
-      "Content-Type": "application/json",
-      'API-KEY': process.env.RIOT_API_KEY,
-    },
-  });
-  const data = await res.json();
+// export async function GET() {
+//   const apiKey = process.env.NEXT_PUBLIC_RIOT_API_KEY;
 
-  return Response.json({data});
-}
+//   if (!apiKey) {
+//     throw new Error("API_KEY가 필요합니다");
+//   }
 
+//   const res = await fetch(
+//     "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion.json",
+//     {
+//       headers: {
+//         //???????여기 headers 타입을 넣는 건가? 왜 빨간 오류가 안 사라지지?
+//         "Content-Type": "application/json",
+//         "API-KEY": apiKey,
+//       },
+//     }
+//   );
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch champion data");
+//   }
+
+//   const data = await res.json();
+
+//   return Response.json({ data });
+// }
