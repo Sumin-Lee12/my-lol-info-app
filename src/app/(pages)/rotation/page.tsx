67540dfb7ba8780d9695f40a12b675f4fec12316
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-19 11:36:18 +0900

    Fix: 로테이션 페이지 사용하지 않는 기능 제거

--- src/app/(pages)/rotation/page.tsx
+++ src/app/(pages)/rotation/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import type { ChampionDetail } from "../../types/Champion";
 import { fetchAllChampionList } from "../../utils/serverApi";
 import Card from "../../components/card";
